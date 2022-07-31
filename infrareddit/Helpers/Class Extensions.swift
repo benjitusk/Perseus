b30b2eda5c6bdb1e68ef30cedcccaf9b75357bac
BenjiTusk <benjitusk1@gmail.com>
2022-07-31 02:33:40 -0400

    added helper for Localized error

--- infrareddit/Helpers/Class Extensions.swift
+++ infrareddit/Helpers/Class Extensions.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 extension URL {
-    subscript(queryParam:String) -> String? {
+    subscript(queryParam: String) -> String? {
         guard let url = URLComponents(string: self.absoluteString) else { return nil }
         return url.queryItems?.first(where: { $0.name == queryParam })?.value
     }
+}
+
+extension String: LocalizedError {
+    /// https://stackoverflow.com/a/40629365/13368672
+    public var errorDescription: String? { return self }
 }
