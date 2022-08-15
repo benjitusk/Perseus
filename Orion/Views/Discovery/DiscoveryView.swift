ebe67295aff1e5075ff809e4f6b1ed9796a2f661
Alberto Delle Donne <alberto.delledonne05@gmail.com>
2022-08-15 10:52:35 +0200

    Discovery UI

---
+++ Orion/Views/Discovery/DiscoveryView.swift
@@ -0,0 +1,36 @@
+//
+//  DiscoveryView.swift
+//  orion
+//
+//  Created by Alberto Delle Donne on 15/08/22.
+//
+
+import SwiftUI
+
+struct DiscoveryView: View {
+    @State var searchText : String = ""
+    let horizontalPadding: CGFloat = 10
+    @State private var selection: Panel? = Panel.discovery
+    var body: some View {
+        NavigationSplitView {
+            DiscoverySidebar(selection: $selection)
+        } detail: {
+            NavigationStack {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("Browse").font(.title2).fontWeight(.bold)
+                        DiscoveryGrid()
+                    }.padding(.horizontal, horizontalPadding)
+                }
+                .navigationTitle("Search")
+                .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+            }
+        }
+    }
+}
+
+struct DiscoveryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoveryView()
+    }
+}
